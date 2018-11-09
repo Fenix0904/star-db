287eb0d924f4d3aa64ffc0e0e5d5c6f656d20100
Svyatoslav Oprysko <opryskosv@gmail.com>
2018-11-09 12:41:24 +0200

    Connected random-planet to API. Refactored model transformation stage.

--- src/services/SwapiService.js
+++ src/services/SwapiService.js
@@ -4,36 +4,75 @@ export default class SwapiService {
 
     async getResource(url) {
         const res = await fetch(`${this._apiBase}${url}`);
-        if (!res.ok) {
-            throw new Error("error");
-        }
         return await res.json();
     };
 
     async getAllPeople() {
         const res = await this.getResource(`/people/`);
-        return res.results;
+        return res.results.map(this._transformPerson);
     }
 
     async getAllPlanets() {
         const res = await this.getResource(`/planets/`);
-        return res.results;
+        return res.results.map(this._transformPlanet);
     }
 
     async getAllStarShips() {
         const res = await this.getResource(`/starships/`);
-        return res.results;
+        return res.results.map(this._transformStarship);
     }
 
-    getPerson(id) {
-        return this.getResource(`/people/${id}/`);
+    async getPerson(id) {
+        const person = await this.getResource(`/people/${id}/`);
+        return this._transformPerson(person);
     }
 
-    getPlanet(id) {
-        return this.getResource(`/planet/${id}/`);
+    async getPlanet(id) {
+        const planet = await this.getResource(`/planets/${id}/`);
+        return this._transformPlanet(planet);
     }
 
-    getStarShip(id) {
-        return this.getResource(`/starship/${id}/`);
+    async getStarShip(id) {
+        const starShip = await this.getResource(`/starships/${id}/`);
+        return this._transformStarship(starShip);
+    }
+
+    _extractId(item) {
+        const idRegExp = /\/([0-9]*)\/$/;
+        return item.url.match(idRegExp)[1];
+    }
+
+    _transformPlanet(planet) {
+        return {
+            id: this._extractId(planet),
+            name: planet.name,
+            population: planet.population,
+            rotationPeriod: planet.rotation_period,
+            diameter: planet.diameter
+        };
+    }
+
+    _transformPerson(person) {
+        return {
+            id: this._extractId(person),
+            name: person.name,
+            gender: person.gender,
+            birthYear: person.birthYear,
+            eyeColor: person.eyeColor
+        };
+    }
+
+    _transformStarship(sratShip) {
+        return {
+            id: this._extractId(sratShip),
+            name: sratShip.name,
+            model: sratShip.model,
+            manufacturer: sratShip.manufacturer,
+            costInCredits: sratShip.costInCredits,
+            length: sratShip.length,
+            crew: sratShip.crew,
+            passengers: sratShip.passengers,
+            cargoCapacity: sratShip.cargoCapacity
+        };
     }
 }
